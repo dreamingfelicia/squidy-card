00baf591e7b51fabe280e44075f6d65184a57555
Bryan Ollendyke <bto108@psu.edu>
2020-06-01 12:01:35 -0400

    changes as seen in video

--- some-card/src/SomeCard.js
+++ some-card/src/SomeCard.js
@@ -1,38 +1,27 @@
-import { html, css, LitElement } from 'lit-element';
+import { html, css, LitElement } from 'lit-element/lit-element.js';
 
 export class SomeCard extends LitElement {
   static get styles() {
-    return css`
+    return [css`
       :host {
         display: block;
+        position: relative;
         color: var(--some-card-text-color, #000);
+        font-family: var(--some-card-font-family, "Times New Roman", Times, serif);
       }
 
-      .container{
-        position: absolute;
+      .container {
+        position: relative;
         display: flex;
         box-shadow: 3px 3px 4px grey;
         border: 10px solid #fff;
-        width: 500px;
         height: 200px;
         transition: all 1s ease-in-out;
       }
-
-      .container:hover{
-        background-color: black;
-        border: 10px solid black;
-        transition: all 1s;
-      }
-
-      .container:hover img{
-        transform: translateX(-300px);
-	      transition-delay: 0.1s;
-      }
-
-      .stack{
+      /** default state */
+      .stack {
         position: absolute;
         z-index: 9;
-        width: 500px;
         height: 200px;
         border: 10px solid transparent;
         top: -10px;
@@ -40,47 +29,33 @@ export class SomeCard extends LitElement {
         opacity: 0;
         overflow: hidden;
       }
-
-      .stack:hover{
-        position: absolute;
-        z-index: 9;
-        opacity: 1;
-      }
-
-      .stack p{
+      .stack ::slotted(*) {
         color: white;
         text-align: center;
         transform: translateY(200px);
 	      transition: all 0.2s linear;
         font-size: 10px;
-        
       }
 
-      .stack:hover p { 
-        opacity:1; 
-        transition-delay: 0.2s;
-        transform: translateY(-10px);
-      }
-
-      .wrapperleft{
+      .wrapper-left {
         position: relative;
         flex: 1;
         overflow: hidden;
         z-index: -1;
       }
 
-      .wrapperright{
+      .wrapper-right {
         position: relative;
         flex: 1.25;
         padding: 0px 10px 0px 10px;
         z-index: -1;
       }
 
-      .text{
+      .text {
         padding: 0px 5px 0px 5px;
       }
 
-      img{
+      img {
         display: block;
         position: absolute;
         width: 100%;
@@ -89,45 +64,82 @@ export class SomeCard extends LitElement {
         transition: all 0.3s ease-in-out; 
       }
 
-      .h1{
-        font-family: "Times New Roman", Times, serif;
+      .default-display {
         text-align: center;
         position: relative;
         font-size: 10px;
       }
 
-
-    `;
+      /** activated state for the element display */
+      :host([active]) .container {
+        background-color: black;
+        border: 10px solid black;
+        transition: all 1s;
+      }
+      :host([active]) .container img {
+        transform: translateX(-300px);
+	      transition-delay: 0.1s;
+      }
+      :host([active]) .stack {
+        position: absolute;
+        z-index: 9;
+        opacity: 1;
+      }
+      :host([active]) .stack ::slotted(*) { 
+        opacity:1; 
+        transition-delay: 0.2s;
+        transform: translateY(-10px);
+      }
+    `];
   }
 
   static get properties() {
     return {
-      img: { type: String },
+      source: { type: String },
+      active: { type: Boolean, reflect: true }
     };
   }
 
   constructor() {
     super();
-    this.img = "https://i.imgur.com/cA9E9G8.png";
+    this.source = "";
+    this.active = false;
+    this.setAttribute('tabindex', 0);
+    // on event
+    this.addEventListener("focusin", this._activeOnEvent);
+    this.addEventListener("mouseover", this._activeOnEvent);
+    // off event
+    this.addEventListener("focusout", this._activeOffEvent);
+    this.addEventListener("mouseout", this._activeOffEvent);
   }
 
+  _activeOnEvent(e) {
+    this.active = true;
+  }
+
+  _activeOffEvent(e) {
+    this.active = false;
+  }
 
   render() {
     return html`
-    <tr>
     <div class="body">
       <div class="container">
-      <div class="stack"><p><slot name="stack"></slot></p></div>
-        <div class="wrapperleft">
-          <img src="${this.img}">
+        <div class="stack">
+          <slot name="details"></slot>
         </div>
-        <div class="wrapperright"><div class="text"><div class="h1"><slot name="attributes"></slot>
+        <div class="wrapper-left">
+          <img src="${this.source}" alt="" />
         </div>
-        </div>
+        <div class="wrapper-right">
+          <div class="text">
+            <div class="default-display">
+              <slot></slot>
+            </div>
+          </div>
         </div>
       </div>
-  </div>
-  </div>
+    </div>
     `;
   }
 }
